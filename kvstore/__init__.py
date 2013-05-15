337f1d7353162efa31ec5a332b0282cb9fe6764e
Jon Brelig <jonbrelig@gmail.com>
2013-05-15 15:11:27 -0700

    Added missing __init__ implementation

---
+++ kvstore/__init__.py
@@ -0,0 +1,33 @@
+from kvstore.managers import TagDescriptor
+
+
+class AlreadyRegistered(Exception):
+    """
+    An attempt was made to register a model more than once.
+    """
+    pass
+
+registry = []
+
+
+def register(model, descriptor_attr='kvstore'):
+    """
+    Sets the given model class up for working with tags.
+    """
+
+    if model in registry:
+        raise AlreadyRegistered("The model '%s' has already been "
+            "registered." % model._meta.object_name)
+    if hasattr(model, descriptor_attr):
+        raise AttributeError("'%s' already has an attribute '%s'. You must "
+            "provide a custom tag_descriptor_attr to register." % (
+                model._meta.object_name,
+                descriptor_attr,
+            )
+        )
+
+    # Add tag descriptor
+    setattr(model, descriptor_attr, TagDescriptor())
+
+    # Finally register in registry
+    registry.append(model)

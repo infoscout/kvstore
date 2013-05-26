0bd67ae2e3d4fac5eb4ce387a3b4528335ea9ec1
Jon Brelig <jonbrelig@gmail.com>
2013-05-26 08:53:39 -0700

    Add version to setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages
+from isc_ops.setup_tools import setup, current_version
 
 setup(name='kvstore',
     packages=find_packages(),  
     description = 'Django allows you to easily tag a django db object with key/value pairs.',
     url = 'http://github.com/infoscout/kvstore',
-    version = '1.0',    
+    version = current_version(),    
     install_requires=[
         'django==1.4',
     ]

ae5f1bcbbaa9c3a6ab1ecb90c29b7d823c7886c5
Jon Brelig <jonbrelig@gmail.com>
2013-12-26 23:17:02 -0800

    Added templates to setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from isc_ops.setup_tools import setup, current_version
 
 setup(name='kvstore',
     packages=find_packages(),  
+    include_package_data=True,
     description = 'Django allows you to easily tag a django db object with key/value pairs.',
     url = 'http://github.com/infoscout/kvstore',
     version = current_version(),    

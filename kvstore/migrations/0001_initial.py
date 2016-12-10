6fbdddbac758ed016c6fd106041f7bf82f6d8440
Lucas Connors <lucas.revolutiontech@gmail.com>
2016-12-09 16:46:11 -0800

    CA-4418 Add support for Django 1.7
    by creating Django 1.7-style migrations

---
+++ kvstore/migrations/0001_initial.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('contenttypes', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Tag',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('object_id', models.PositiveIntegerField()),
+                ('key', models.CharField(max_length=32, db_index=True)),
+                ('value', models.TextField()),
+                ('content_type', models.ForeignKey(to='contenttypes.ContentType')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AlterUniqueTogether(
+            name='tag',
+            unique_together=set([('content_type', 'object_id', 'key')]),
+        ),
+    ]

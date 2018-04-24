3c567fd000a253b3d5995fb9fa0dd256ef697c30
Josie Lipkin <josie.lipkin@infoscoutinc.com>
2018-04-24 22:08:43 +0000

    created a command class to run tests

--- setup.py
+++ setup.py
@@ -1,8 +1,47 @@
-from setuptools import find_packages, setup
+from setuptools import Command,find_packages, setup
 
 
 with open('VERSION', 'r') as f:
     version = f.read().strip()
+
+
+class TestCommand(Command):
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import django
+        from django.conf import settings
+        from django.core.management import call_command
+
+        settings.configure(
+            DATABASES={
+                'default': {
+                    'NAME': ':memory:',
+                    'ENGINE': 'django.db.backends.sqlite3',
+                },
+            },
+            # DEFAULT_INDEX_TABLESPACE='',
+            INSTALLED_APPS=(
+                'django.contrib.auth',
+                'django.contrib.contenttypes',
+                # 'django.contrib.sessions',
+                'kvstore',
+                'kvstore.tests',
+            ),
+            MIDDLEWARE=(),
+            # MIDDLEWARE_CLASSES=(),  # Django < 1.10
+            # ROOT_URLCONF='saml_service_provider.urls',
+            # AUTHENTICATION_BACKENDS=['isc_saml.auth_backend.ISCSAMLBackend']
+        )
+        django.setup()
+        call_command('test', 'kvstore')
 
 
 setup(
@@ -14,5 +53,9 @@ setup(
     version=version,
     install_requires=[
         'Django>=1.8',
-    ]
+    ],
+    tests_require=[
+        'mock',
+    ],
+    cmdclass={'test': TestCommand}
 )

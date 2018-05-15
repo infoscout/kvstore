5f3518522d3e5d415c4a4e6d3c085b1de50cd99d
Josie Lipkin <josie.lipkin@infoscoutinc.com>
2018-05-15 16:55:25 +0000

    added unicode support to all the python modules

--- kvstore/accessor.py
+++ kvstore/accessor.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 class TagAccessor(object):
     """
     An abstraction layer for the kvstore attached
--- kvstore/tests/test_register.py
+++ kvstore/tests/test_register.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 import kvstore
 from kvstore.tests.models import Article
 from kvstore.tests.utils import KVStoreBaseTestCase

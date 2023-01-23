bfff665c0a0a73ee4bc2d2f1f032bf7cf9f97db3
Ryan Krabath <ryan.krabath@numerator.com>
2023-01-23 17:15:31 -0600

    CA-14551: Reconfigure project for newer Djangos

--- kvstore/models.py
+++ kvstore/models.py
@@ -4,12 +4,10 @@ from __future__ import unicode_literals
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
 
 from kvstore.managers import TagManager
 
 
-@python_2_unicode_compatible
 class Tag(models.Model):
 
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
